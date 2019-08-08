bf78bfcac6b7f421958eaf48a36ee55720e47e24
Abhishek Wani <43184560+whoami02@users.noreply.github.com>
2019-08-08 21:14:20 +0530

    A way to convert string to Int.....

--- word_to_integer.cpp
+++ word_to_integer.cpp
@@ -16,11 +16,13 @@ int main(){
     }
     int M[10000];
     for(i=0; i<=count; i++){
-        // cout<<J[i];
-        do{
-            (J[i]%2 == 1) ? (M[i] = 1) : (M[i] = 0);
-            J[i] /= 2; 
-        }while(J[i] != 1);
+        cout<<J[i];
+        // do{
+        //     (J[i]%2 == 1) ? (M[i] = 1) : (M[i] = 0);
+        //     J[i] /= 2; 
+        // }while(J[i] != 1);
     }
-    
+    // for(i=0; i<=count; i++){
+    //     cout << M[i];
+    // }
 }   
