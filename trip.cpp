9165d4e1124d35366cd0c15cc1f01c55f076c03a
Abhishek Wani <43184560+whoami02@users.noreply.github.com>
2019-08-08 21:05:21 +0530

    Update trip.cpp
    trip solved..
    Dilema remaining

--- trip.cpp
+++ trip.cpp
@@ -1,30 +1,33 @@
+// l_10137
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n; cin >> n;
-    while(n){
-    vector<float>m;
-    vector<float>k;
-    int sum = 0;
-    // int a[n];
-    for(int i=0; i<n; i++){
-        float x; cin >>x;
-        m.push_back(x);
-    }
-    sort(m.begin(), m.end());
-    // float x = m.back();
-    // for(int i=0; i<n; i++){
-    //     float y = x-m[i];
-    //     k.push_back(y);
-    //     sum += y;
-    // }
-    int y = m.size();
-    // sort(k.begin(), k.end());
-    // cout << ( (y&1) ? (m[y/2 + 1]) :(m[(y/2+(y/2 + 1))/2]) ) << endl;
-    cout << (m[y/2] - m[y/2-1]) << endl;
-    if(n==0) break;
-    else continue;
-    }
-    // return 0;
     
+    while(1){
+        int n; cin >> n;
+        if(n == 0) break;
+        vector<float>m;
+        vector<float>k;
+        int sum = 0;
+        // int a[n];
+        for(int i=0; i<n; i++){
+            float x; cin >>x;
+            m.push_back(x);
+        }
+        sort(m.begin(), m.end());
+        
+        sum = accumulate(m.begin(), m.end(), 0.0);
+        float avg = sum/m.size();
+        for(int i=0; i<n; i++){
+            if(m[i] < avg) {
+                float x = avg-m[i] ;
+                k.push_back(x);
+            }
+            else continue;
+        }
+        avg = accumulate(k.begin(), k.end(), 0.0);
+        cout << avg << endl;
+    }
+        
 }
