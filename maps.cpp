e8e151dfdeb812d376836526fce65f5f2c5785cf
Abhishek Wani <43184560+whoami02@users.noreply.github.com>
2019-08-16 23:33:35 +0530

    Create maps.cpp

---
+++ maps.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  int q, type; cin >> q; 
+  string name;
+  map<string,int> m; 
+  for (int i(0), mark; i<q; ++i)
+  {
+    cin >> type >> name;
+    if (type == 1){
+      cin >> mark;
+      m[name] += mark;
+    }
+    else if (type == 2)
+      m.erase(name);
+    else
+      cout << m[name] << endl;
+  }
+  return 0;
+}
